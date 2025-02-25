46422396bccbf79b7905f2c538cab831b65611da
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-25 15:35:38 -0500

    segundo commit

---
+++ pack3/__init__.py
@@ -0,0 +1,1 @@
+from pack3.funcionppal import ndfaleatorio
--- pack3/funcionppal.py
+++ pack3/funcionppal.py
@@ -1,1 +1,27 @@
 import pandas as pd
+
+
+import random
+
+numero_aleatorio = random.randint(10, 30)  # Número entre 1 y 100 (ambos incluidos)
+print(numero_aleatorio) 
+
+
+# Crear un DataFrame
+data = {
+    "Categoría": ["A", "B", "C", "D"],
+    "Cantidad": [numero_aleatorio, 20, 30, 40]
+}
+df = pd.DataFrame(data)
+
+
+print(df["Cantidad"].sum())
+
+def ndfaleatorio():
+
+    df = pd.DataFrame(data)
+    # Mostrar el DataFrame
+    return(df["Cantidad"].sum())
+
+if __name__ == "__main__":
+    print("Ejecucion desde el sistema")
