6a7e0246bc41ec592e95db732cca64b78a9362e4
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-25 15:48:58 -0500

    para cargar el archivo stup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paquetetres",  # Nombre del paquete
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Lista de dependencias si las tienes
+    author="Juan Paez",
+    author_email="jcpaezl@gmail.com",
+    description="Descripción breve del paquete",
+    url="https://github.com/juancarlospaez/paquetetres",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
