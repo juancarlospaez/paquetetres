787002c65fbb5a0d6d77b9706797ea22729043bf
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-26 10:12:32 -0500

    Se actualiza el setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paquetetres",  # Nombre del paquete
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[],  # Lista de dependencias si las tienes
     author="Juan Paez",
