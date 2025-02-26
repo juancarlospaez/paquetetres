272d08526436440c47fcc77c42b7ca90db5b29f1
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-25 19:13:32 -0500

    se actualiza semilla aleatoria

--- pack3/funcionppal.py
+++ pack3/funcionppal.py
@@ -3,21 +3,24 @@ import pandas as pd
 
 import random
 
-numero_aleatorio = random.randint(10, 30)  # Número entre 1 y 100 (ambos incluidos)
-print(numero_aleatorio) 
-
-
-# Crear un DataFrame
-data = {
-    "Categoría": ["A", "B", "C", "D"],
-    "Cantidad": [numero_aleatorio, 20, 30, 40]
-}
-df = pd.DataFrame(data)
-
-
-print(df["Cantidad"].sum())
 
 def ndfaleatorio():
+
+
+    numero_aleatorio = random.randint(10, 30)  # Número entre 1 y 100 (ambos incluidos)
+    print(numero_aleatorio) 
+
+
+    # Crear un DataFrame
+    data = {
+        "Categoría": ["A", "B", "C", "D"],
+        "Cantidad": [numero_aleatorio, 20, 30, 40]
+    }
+    df = pd.DataFrame(data)
+
+
+    print(df["Cantidad"].sum())
+
 
     df = pd.DataFrame(data)
     # Mostrar el DataFrame
