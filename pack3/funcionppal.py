d28d23ba4e8b7d8ba7a17fc61a9f6f3db9d43cdd
JUAN C PAEZ L <jcpaezl@gmail.com>
2025-02-25 19:45:32 -0500

    ajuste verificacion tema comillas

--- pack3/funcionppal.py
+++ pack3/funcionppal.py
@@ -1,28 +1,19 @@
 import pandas as pd
-
-
 import random
 
-
 def ndfaleatorio():
-
-
     numero_aleatorio = random.randint(10, 30)  # Número entre 1 y 100 (ambos incluidos)
     print(numero_aleatorio) 
 
-
     # Crear un DataFrame
-    data = {
-        "Categoría": ["A", "B", "C", "D"],
-        "Cantidad": [numero_aleatorio, 2, 3, 4]
-    }
+    data = {"Categoría": ["A", "B", "C", "D"],
+            "Cantidad": [numero_aleatorio, 2, 3, 4]
+           }
 
     df = pd.DataFrame(data)
     Variable = df["Cantidad"].sum()
-    return(f"SUMA columna {Variable}")
 
-
-
+    return(f"SUMA columna {int(Variable)}")
 
 if __name__ == "__main__":
 
